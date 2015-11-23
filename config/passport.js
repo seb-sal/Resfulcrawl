8f29249e4ccbefaf8045ff5e2f6548d4133d124f
Adam Allgood <allgoodadam@gmail.com>
2015-11-23 14:21:44 -0800

    working on Oauth still, trying to figure out how to acces profile image

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,25 @@
 var GoogleStrategy = require('passport-google-oauth').Oauth2Strategy;
-var User = require('../models/user')
+var User = require('../models/user');
 
 module.exports = function(passport) {
 
-  passport.use(new GoogleStrategy)
+  passport.use(new GoogleStrategy({
+      clientID: process.env.API_GOOGLE_PLUS,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK
+    },
+    function(accessToken, refreshToken, profile, done) {
+      User.findOne({'googleId': profile.id}, function(err, user) {
+        if(err) return done(err);
+        if(user) {
+          return done(null, user);
+        } else {
+          var newUser = new User({
+
+          });
+        }
+      });
+    }
+  ));
 
 }
--- models/user.js
+++ models/user.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
-var userSchema = new mongoose.Schema({
-  name:   String,
-  handle: String
+var userSchema = new mongooose.Schema({
+  prof_name: String,
+  prof_email: String,
+  prof_picture: String
 });
 
 var User = mongoose.model('User', userSchema);
