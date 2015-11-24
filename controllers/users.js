c3d705967c2cf94e0e5086374b99527e52b39f95
Sebastian Salazar <sebastian@sasa.guru>
2015-11-23 20:17:57 -0800

    add req.user in user.js file

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ var User = require("../models/user");
 var index = function(req, res, next){
 
   User.find({}, function(error, users){
-    res.render('users/index', {users: users});
+    res.render('users/index', {users: users, user: req.user});
   });
 };
 
