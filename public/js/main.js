c1026f4b272b9871afbd098f8e37ce79c6e5c69a
Joey Lin <pujoey@gmail.com>
2015-11-24 17:00:29 -0800

    use AJAX to add crawls to index on load

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,35 @@
 console.log('JS loaded!');
 
-var rootUrl = '/crawls/';
-
 $(document).ready(function () {
-
-  var $crawlDetail = $('#crawl-detail');
   var $mainContent = $('#main-content');
-
+  var $crawlDetail = $('.crawl-detail');
 
   //compile all templates
   var crawlDetailTemplate = _.template($('#crawlDetailTemplate').html());
 
+  $.get('/crawls', function(crawls) {
+    console.log(crawls);
 
-  // attach event listeners
-  $('.crawl').on('click', showCrawlDetails);
+    crawls.forEach(function(crawl) {
+      var crawlHTML = crawlDetailTemplate({crawl: crawl});
+      $crawlDetail.append(crawlHTML);
+      $crawlDetail.append(crawlHTML);
+      $crawlDetail.append(crawlHTML);
+    });
+  });
 
 
-
-
-
+  // $crawlDetail.html(crawlDetailTemplate({crawl: data[0]}));
+  // $crawlDetail.fadeIn();
+  // // attach event listeners
+  // $('.crawl').on('click', showCrawlDetails);
 
   // view crawl details
-  function showCrawlDetails(evt) {
-    $mainContent.fadeOut();
-    var crawlId = $(evt.target).attr('data-crawl-id');
-    $.get(rootUrl + crawlId, function(data) {
-      $crawlDetail.html(crawlDetailTemplate({crawl: data[0]}));
-      $crawlDetail.fadeIn();
-    });
-  }
+  // function showCrawlDetails(evt) {
+    // $mainContent.fadeOut();
+    // var crawlId = $(evt.target).attr('data-crawl-id');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // }
 
   // $.get(rootUrl)
   // .success(function(data) {
