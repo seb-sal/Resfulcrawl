0a107fe30ecd1bd853415a78846a01cda3677d15
Adam Allgood <allgoodadam@gmail.com>
2015-11-28 14:34:12 -0800

    pulling

--- config/routes.js
+++ config/routes.js
@@ -12,11 +12,7 @@ var yelpController    = require('../controllers/yelp');
 module.exports = function(app, passport) {
 
   function isAuthenticated(req, res, next) {
-
-    // do any checks you want to in here
-
     // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
-    // you can do this however you want with whatever variables you set up
     if (req.user)
         return next();
 
