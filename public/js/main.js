32c932f663710dcbc4f91c550e608afa374c410c
Ferdie Quizon <fquizon@gmail.com>
2015-11-28 13:33:02 -0800

    overwriting with current changes

--- public/js/main.js
+++ public/js/main.js
@@ -53,3 +53,37 @@ $(document).ready(function () {
     });
   });
 });
+
+
+var map;
+var currentLocation;
+function initMap() {
+ if(navigator.geolocation) {
+   browserSupportFlag = true;
+   navigator.geolocation.getCurrentPosition(function(position) {
+     map = new google.maps.Map(document.getElementById('map'), {
+       center: {lat: position.coords.latitude, lng: position.coords.longitude},
+       zoom: 14
+     });
+     currentLocation = new google.maps.Marker({
+       position: {lat: position.coords.latitude, lng: position.coords.longitude},
+       map: map,
+       title: 'Hello World!'
+ });
+
+   })
+ }
+}
+
+
+
+
+// if(navigator.geolocation) {
+//   browserSupportFlag = true;
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+//     map.setCenter(initialLocation);
+//   }, function() {
+//     handleNoGeolocation(browserSupportFlag);
+//   });
+// }
