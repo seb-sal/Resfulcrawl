5a30344d7d36979e083df0703a4096680f3ac6ce
Sebastian Salazar <sebastian@sasa.guru>
2015-11-28 19:57:13 -0800

    changed http to https in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -63,7 +63,7 @@ function initMap() {
   var bounds = new google.maps.LatLngBounds();
   //ajax call to google api for geocoding
   for (var x = 0; x < addresses.length; x++) {
-      $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+      $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
           var p = data.results[0].geometry.location;
           var latlng = new google.maps.LatLng(p.lat, p.lng);
 
